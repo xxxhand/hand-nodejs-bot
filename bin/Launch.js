85bcaa14f747f8ff47bd6266355af986aad1d8d6
HandsJWu <handsjwu@fih-foxconn.com>
2018-03-28 14:34:38 +0800

    fix

--- bin/Launch.js
+++ bin/Launch.js
@@ -1,16 +1,16 @@
 const app = require('./../bootstrap/App')
 
-app().listen(process.env.PORT || 8080, err => {
-    if (err) {
-        console.error(err)
-        return process.exit(0)
-    }
-    console.log(`Server up on port 8080`)
-})
-
-// const server = require('http').createServer(app()).listen(appConfig.port)
-// server.on('listening', () => console.log(`Server up on ${server.address().address}:${server.address().port}`))
-// server.on('error', err => {
-//     console.error(err)
-//     process.exit(1)
+// app().listen(process.env.PORT || 8080, err => {
+//     if (err) {
+//         console.error(err)
+//         return process.exit(0)
+//     }
+//     console.log(`Server up on port 8080`)
 // })
+
+const server = require('http').createServer(app()).listen(process.env.PORT || appConfig.port)
+server.on('listening', () => console.log(`Server up on ${server.address().address}:${server.address().port}`))
+server.on('error', err => {
+    console.error(err)
+    process.exit(1)
+})
