e5116a97da2e1a0d8203b632d530827b67eee5fc
HandsJWu <handsjwu@fih-foxconn.com>
2018-03-28 14:30:26 +0800

    fix

--- bin/Launch.js
+++ bin/Launch.js
@@ -1,6 +1,12 @@
 const app = require('./../bootstrap/App')
 
-app().listen(8080)
+app().listen(8080, err => {
+    if (err) {
+        console.error(err)
+        return process.exit(0)
+    }
+    console.log(`Server up on port 8080`)
+})
 
 // const server = require('http').createServer(app()).listen(appConfig.port)
 // server.on('listening', () => console.log(`Server up on ${server.address().address}:${server.address().port}`))
