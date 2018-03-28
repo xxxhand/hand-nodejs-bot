35eaafe26aa5fe0c7eb9d74000e6edeca444fc22
HandsJWu <handsjwu@fih-foxconn.com>
2018-03-28 14:01:59 +0800

    cancel domain name

--- bin/Launch.js
+++ bin/Launch.js
@@ -1,6 +1,6 @@
 const app = require('./../bootstrap/App')
 
-const server = require('http').createServer(app()).listen(appConfig.port, appConfig.domain)
+const server = require('http').createServer(app()).listen(appConfig.port)
 server.on('listening', () => console.log(`Server up on ${server.address().address}:${server.address().port}`))
 server.on('error', err => {
     console.error(err)
