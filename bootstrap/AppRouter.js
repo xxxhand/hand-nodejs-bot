5cc11a04028942004dec79c745e13efd93d515fb
HandsJWu <handsjwu@fih-foxconn.com>
2018-03-29 13:27:45 +0800

    add line service

--- bootstrap/AppRouter.js
+++ bootstrap/AppRouter.js
@@ -2,6 +2,21 @@ const express = require('express')
 const line = require('@line/bot-sdk')
 const applicationIndex = require('./../applications/index')
 
+
+const lineClient = new line.Client(lineConfig)
+const handleLineEvent = event => {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        return Promise.resolve(null)
+    }
+
+    return lineClient.replyMessage(event.replyToken, {
+        type: 'text',
+        text: event.message.text
+    })
+}
+
+
+
 function initMainRouter() {
     const mainRouter = express.Router()
     const lineConfig = {
@@ -9,7 +24,10 @@ function initMainRouter() {
         channelSecret: AppConfig.lineSettings.channelSecret
     }
     const lineApp = new applicationIndex.LineApplication()
-    mainRouter.post('/linehook', line.middleware(lineConfig), lineApp.finalHandle)
+    mainRouter.post('/linehook', line.middleware(lineConfig), (req, res) => {
+        Promise.all(req.body.events.map(handleLineEvent))
+        .then(x => res.json(x))
+    })
 
     mainRouter.all('/', (req, res) => {
         res.send('Hello world')
