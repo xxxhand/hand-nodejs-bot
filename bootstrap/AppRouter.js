ae74d1a437201f8f780c98f6ed5762a3840fd268
HandsJWu <handsjwu@fih-foxconn.com>
2018-03-29 13:17:40 +0800

    add line service

--- bootstrap/AppRouter.js
+++ bootstrap/AppRouter.js
@@ -1,11 +1,15 @@
 const express = require('express')
+const line = require('@line/bot-sdk')
 const applicationIndex = require('./../applications/index')
 
 function initMainRouter() {
     const mainRouter = express.Router()
-
+    const lineConfig = {
+        channelAccessToken: AppConfig.lineSettings.channelAccessToken,
+        channelSecret: AppConfig.lineSettings.channelSecret
+    }
     const lineApp = new applicationIndex.LineApplication()
-    mainRouter.post('/linehook', lineApp.middleware, lineApp.finalHandle)
+    mainRouter.post('/linehook', line.middleware(lineConfig), lineApp.finalHandle)
 
     mainRouter.all('/', (req, res) => {
         res.send('Hello world')
